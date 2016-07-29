9ec389ee27c60425a21da1f59378e9485cb51a4f
catameojc <jc.catameo@dot.ph>
2016-07-29 15:04:07 +0800

    Test

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,14 +20,14 @@ set :assets_roles, [:web, :app]
 
 after 'deploy:publishing', 'deploy:restart'
 
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
-
 #namespace :deploy do
-#  on roles :all do
-#      execute :service, "unicorn upgrade"
+#  task :restart do
+#    invoke 'unicorn:restart'
 #  end
 #end
+
+namespace :deploy do
+  on roles :all do
+      execute :service, "unicorn upgrade"
+  end
+end
