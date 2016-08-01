a1c4b3c5d25d991933b7239af8297c13faf9ff37
catameojc <jc.catameo@dot.ph>
2016-08-01 11:56:26 +0800

    Test

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     on roles :all do
-      execute "service unicorn upgrade"
+      execute "/etc/init.d/unicorn upgrade"
     end
   end
 end
